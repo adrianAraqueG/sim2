47a502fbef34ba3d0ec1678d442c89b61e0e7166
adrianAraqueG <adrian.kanox.2003@gmail.com>
2023-07-17 21:05:29 -0500

    f1

--- front/js/app.js
+++ front/js/app.js
@@ -4,158 +4,330 @@
 import DB from './clases.js';
 let departamentos = [];
 let ciudades = [];
-let inmuebles = [];
+let editando = false;
 console.log('start');
 
-document.addEventListener('DOMContentLoaded', async() => {
+document.addEventListener('DOMContentLoaded', async () => {
     await ActualizarDB();
+    actualizarDepartamentos();
+    imprimirDepartamentos();
+    imprimirCiudades();
 
 });
 
 /**
  * MAIN
  */
-const form = document.querySelector('#form');
-form.addEventListener('submit', e => {
+const formCiudad = document.querySelector('#form-ciudad');
+formCiudad.addEventListener('submit', e=>{
     e.preventDefault();
-
-    anadirInmueble();
+    addCiudad();
 });
-const nCiudad = document.querySelector('#nuevaCiudad');
-const nDep = document.querySelector('#nuevoDepartamento');
-nDep.addEventListener('click', () => { // Pequeña validación para los checkbox
-    if (nDep.checked === true) {
-        nCiudad.checked = true;
-        nCiudad.setAttribute('disabled', 'disabled');
-    } else if (nDep.checked === false && nCiudad.getAttribute('disabled')) {
-        nCiudad.removeAttribute('disabled');
-        nCiudad.checked = false;
+const formDep = document.querySelector('#form-dep');
+formDep.addEventListener('submit', e=>{
+    e.preventDefault();
+    addDepartamento();
+});
+
+
+const btnCMCiudad = document.querySelector('#cerrar-modal-ciudad');
+btnCMCiudad.addEventListener('click', ()=>{
+    if(editando != false){
+        // Revierto los cambios en los titulos
+        document.querySelector('#modal-ciudad-title').textContent = 'Crear Ciudad'
+        document.querySelector('#form-button-ciudad').textContent = 'Crear';
+
+        editando = false;
     }
-})
 
-
-const btnCloseModal = document.querySelector('#cerrar-modal');
-btnCloseModal.addEventListener('click', () => {
-    form.reset();
+    formCiudad.reset();
 });
+const btnCMDep = document.querySelector('#cerrar-modal-dep');
+btnCMDep.addEventListener('click', ()=>{
+    if(editando != false){
+        // Revierto los cambios en los titulos
+        document.querySelector('#modal-dep-title').textContent = 'Crear Departamento'
+        document.querySelector('#form-button-dep').textContent = 'Crear';
+
+        editando = false;
+    }
+
+    formDep.reset();
+});
+
+
+async function addCiudad(){
+    const depId = document.querySelector('#departamentos').value;
+    const nombreCiudad = document.querySelector('#nombreCiudad').value;
+    const imagenUrl = document.querySelector('#imagenUrl').value;
+    const lat = document.querySelector('#lat').value;
+    const lon = document.querySelector('#lon').value;
+
+    let promises = [];
+
+    let nuevaCiudad = {
+        departamentoId: parseInt(depId),
+        nomCiudad: nombreCiudad,
+        imagen: imagenUrl,
+        coordenadas: {lat: parseInt(lat), lon: parseInt(lon)}
+    }
+
+    if (editando) {
+        console.log(editando);
+        promises.push(DB.update('Ciudades', nuevaCiudad, editando));
+        promises.push(ActualizarDB());
+    } else {
+        promises.push(DB.create('Ciudades', nuevaCiudad));
+        promises.push(ActualizarDB());
+    }
+
+    await Promise.all(promises);
+
+    alert(editando === false ? '¡Ciudad agregada con éxito!': '¡Cambios guardados con éxito!');
+    btnCMCiudad.click();
+
+    editando =  false;
+
+    imprimirCiudades();
+    imprimirDepartamentos();
+}
+
+async function addDepartamento(){
+    const depNombre = document.querySelector('#nombreDep').value;
+
+    let promises = [];
+
+    if (editando) {
+        console.log(editando);
+        promises.push(DB.update('Departamentos', {nomDepartamento: depNombre}, editando));
+        promises.push(ActualizarDB());
+    } else {
+        promises.push(DB.create('Departamentos', {nomDepartamento: depNombre}));
+        promises.push(ActualizarDB());
+    }
+
+    await Promise.all(promises);
+
+    alert(editando === false ? 'Departamento agregado con éxito!': '¡Cambios guardados con éxito!');
+    btnCMDep.click();
+
+    editando =  false;
+
+    imprimirCiudades();
+    imprimirDepartamentos();
+    actualizarDepartamentos();
+}
 
 
 /**
- * FUNCTIONS
+ * FUNCIONES
  */
-async function anadirInmueble() {
-    const precio = document.querySelector('#precio').value;
-    const habitaciones = document.querySelector('#habitaciones').value;
-    const banos = document.querySelector('#banos').value;
-    const metros = document.querySelector('#metros').value;
-    const direccion = document.querySelector('#direccion').value;
-    const ciudad = document.querySelector('#ciudad').value;
-    const departamento = document.querySelector('#departamento').value;
+function cargarCiudad(id){
+    editando = id;
+
+    document.querySelector('#modal-ciudad-title').textContent = 'Editando Ciudad'
+    document.querySelector('#form-button-ciudad').textContent = 'Guardar Cambios';
+
+    actualizarDepartamentos();
+
+    ciudades.forEach(ci => {
+        if (ci.id === id) {
+            document.querySelector('#departamentos').value = ci.departamentoId,
+            nombreCiudad.value = ci.nomCiudad;
+            imagenUrl.value = ci.imagen;
+            lat.value = ci.coordenadas.lat;
+            lon.value = ci.coordenadas.lon;
+        }
+    });
+
+}
+window.cargarCiudad = cargarCiudad;
+
+async function eliminarCiudad(id){
+    await DB.delete('Ciudades', id);
+    await ActualizarDB();
+    imprimirCiudades();
+    imprimirDepartamentos();
+    alert('Eliminado Correctamente');
+
+}
+window.eliminarCiudad = eliminarCiudad;
+
+function cargarDep(id){
+    editando = id;
+
+    document.querySelector('#modal-dep-title').textContent = 'Editando Departamento'
+    document.querySelector('#form-button-dep').textContent = 'Guardar Cambios';
+
+    // Listar ciudades del departamento
+    const listaCiudades = document.querySelector('#lista-ciudades');
+    listaCiudades.innerHTML = '';
+
+    ciudades.forEach(ci =>{
+        if(ci.departamentoId === id){
+            listaCiudades.innerHTML += `
+            <tr>
+                <td>${ci.id}</td>
+                <td>${ci.nomCiudad}</td>
+            </tr>
+            `;
+        }
+    });
+
+    departamentos.forEach(dep => {
+        if (dep.id === id) {
+            nombreDep.value = dep.nomDepartamento;
+        }
+    });
+}
+window.cargarDep = cargarDep;
+
+async function eliminarDep(id){
 
     const promises = [];
-
-    // Validación
-    if (nCiudad.value) {
-        // Buscar Departamento
-        let dId = false;
-        departamentos.forEach(async dep =>{
-            if(departamento == dep.nombre){
-                dId = dep.id
-                // Crear Ciudad
-                promises.push(DB.create('ciudades', { nombre: ciudad, departamentoID: dId }));
-                // Actualizar DB
-                promises.push(ActualizarDB()); 
-            }else{
-                // Manejo de errores
-                alert('NO DEPARTAMENTO');
-            }
-        });
-        
-    }
-    else if (nDep.value) {
-        // Crear departamento
-        let isDep = false; 
-        departamento.forEach(dep =>{
-            if(departamento === dep.nombre){
-                isDep = true;
-                alert("DEP YA EXISTE");
-                return;
-            }
-        })
-
-        if(!isDep){
-            promises.push(DB.create('departamentos', {nombre: departamento}));
-            promises.push(ActualizarDB());
+    //Eliminar ciudades del departamento
+    ciudades.forEach(ci =>{
+        if(ci.departamentoId === id){
+            promises.push(DB.delete('Ciudades', ci.id));
         }
+    });
 
-        // Buscar Departamento
-        let dId = false;
-        departamentos.forEach(async dep =>{
-            if(departamento == dep.nombre){
-                dId = dep.id
-                // Crear Ciudad
-                promises.push(DB.create('ciudades', { nombre: ciudad, departamentoID: dId }));
-                // Actualizar DB
-                promises.push(ActualizarDB()); 
-            }else{
-                // Manejo de errores
-                console.log('NO DEPARTAMENTO');
-            }
-        });
+    //Eliminar departamento
+    promises.push(DB.delete('Departamentos', id));
+    promises.push(ActualizarDB());
 
-    }
-
-    // Esperar a que las acciones anteriores terminen
-    await Promise.all(promises);
-
-    console.log(ciudades)
-    const nuevoInmueble = {
-        precio,
-        habitaciones,
-        banos,
-        metros,
-        direccion,
-        ciudad,
-        departamento
-    }
-
-    // Guardar en DB
-    if((ciudades.filter(ci => ci === ciudad)).length !== 0){
-        await DB.create(nuevoInmueble);
-    }else{
-        alert('LA CIUDAD NO EXISTE');
-    }
+    Promise.all(promises);
     
+    imprimirCiudades();
+    imprimirDepartamentos();
+    actualizarDepartamentos();
+    alert('Eliminado Correctamente');
 }
+window.eliminarDep = eliminarDep;
 
-function eliminarJuego(id) {
-    juegos = juegos.filter(juego => juego.id !== id);
-    LS.setItem('juegos', JSON.stringify(juegos)); //Actualizar LS
-    imprimirTabla(juegos);
-}
+function imprimirDepartamentos(){
+    const divDepartamentos = document.querySelector('#show-dep');
 
+    divDepartamentos.innerHTML = '';
 
+    departamentos.forEach(dep =>{
 
-function imprimirTabla(datos) {
-    const table = document.querySelector('#contenido-tabla');
-    table.innerHTML = '';
-
-    datos.forEach(juego => {
-        table.innerHTML += `
-        <tr>
-            <td>${juego.id}</td>
-            <td>${juego.nombre}</td>
-            <td>${juego.categoria}</td>
-            <td>${juego.valorLicencia} USD</td>
-            <td>${juego.puntos}</td>
-            <td>
-                <div class="d-flex align-items-center">
-                    <button class="btn btn-danger" onclick="eliminarJuego(${juego.id})">
-                        <i class="bi bi-trash"></i>
-                    </button>
+        divDepartamentos.innerHTML += `
+        <div class="col">
+            <div class="d-flex justify-content-between bgc-lapiz text-light p-3 rounded">
+                <h6 class="fs-3">${dep.nomDepartamento}<br><span class="--tiny-text text-light">Ciudades: ${ciudades.filter(ci => ci.departamentoId === dep.id).length}</span></h6>
+                <div class="align-self-end me-2">
+                    <a href="#" onclick="cargarDep(${dep.id})" data-bs-toggle="modal" data-bs-target="#abrir-modal-dep"><i
+                            class="bi bi-pencil-square fs-4 text-light me-1"></i></a>
+                    <a href="#" onclick="eliminarDep(${dep.id})"><i class="bi bi-trash-fill fs-4 text-light"></i></a>
                 </div>
-            </td>
-        </tr>
+            </div>
+        </div>
+        `;
+    });
+
+    // Botón añadir
+    divDepartamentos.innerHTML += `
+    <div class="col d-flex justify-content-center align-items-center">
+        <a class="m-auto d-flex justify-content-center align-items-center" href="" data-bs-toggle="modal"
+            data-bs-target="#abrir-modal-dep">
+            <div class="add-dep">
+                <i class="bi bi-plus-circle-fill"></i>
+            </div>
+        </a>
+    </div>
+    `;
+
+}
+
+async function imprimirCiudades(){
+    const divCiudades = document.querySelector('#show-ciudades');
+
+    console.log('Imprimiendo');
+
+    divCiudades.innerHTML = '';
+
+    ciudades.forEach(ci =>{
+        DB.getWeather(ci.nomCiudad)
+        .then(res => {
+            if(res.cod == 404){
+                divCiudades.innerHTML += `
+                <div class="col">
+                    <div class="card m-auto mb-3 shadow" style="width: 18rem;">
+                        <div class="--c-header" style="background-image: url('${ci.imagen}');">
+                            
+                        </div>
+                        <hr class="m-0">
+                        <div class="card-body">
+                            <div class="d-flex flex-wrap justify-content-between p-0 m-0">
+                                <div class="">
+                                    <h5 class="fs-5 mb-0">${ci.nomCiudad},</h5>
+                                    <p class="fs-6 mb-3">${departamentos.filter(dep => ci.departamentoId === dep.id)[0].nomDepartamento}</p>
+                                </div>
+                                <img src="./assets/img/Clear.png" alt="" width="50px" height="50px"><span class="--tiny-text">20° C</span>
+                            </div>
+                            <div class="align-self-end me-2">
+                                <a href="#" onclick="cargarCiudad(${ci.id})" data-bs-toggle="modal" data-bs-target="#abrir-modal-ciudad">
+                                    <i class="bi bi-building-fill-gear fs-4 text-secondary me-1"></i>
+                                </a>
+                                <a href="#" onclick="eliminarCiudad(${ci.id})" ><i class="bi bi-building-fill-dash fs-4 text-danger"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                `;
+            }else{
+                divCiudades.innerHTML += `
+                <div class="col">
+                    <div class="card m-auto mb-3 shadow" style="width: 18rem;">
+                        <div class="--c-header" style="background-image: url('${ci.imagen}');">
+                            
+                        </div>
+                        <hr class="m-0">
+                        <div class="card-body">
+                            <div class="d-flex flex-wrap justify-content-between p-0 m-0">
+                                <div class="">
+                                    <h5 class="fs-5 mb-0">${ci.nomCiudad},</h5>
+                                    <p class="fs-6 mb-3">${departamentos.filter(dep => ci.departamentoId === dep.id)[0].nomDepartamento}</p>
+                                </div>
+                                <img src="./assets/img/${res.weather[0].main}.png" alt="" width="50px" height="50px"><span class="--tiny-text">${res.main.temp}° C</span>
+                            </div>
+                            <div class="align-self-end me-2">
+                                <a href="#" onclick="cargarCiudad(${ci.id})" data-bs-toggle="modal" data-bs-target="#abrir-modal-ciudad">
+                                    <i class="bi bi-building-fill-gear fs-4 text-secondary me-1"></i>
+                                </a>
+                                <a href="#" onclick="eliminarCiudad(${ci.id})" ><i class="bi bi-building-fill-dash fs-4 text-danger"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                `;
+            }
+            
+            });
+    });
+
+    divCiudades.innerHTML = `
+    <div class="col d-flex justify-content-center align-items-center">
+        <a class="m-auto d-flex justify-content-center align-items-center" href="" data-bs-toggle="modal"
+            data-bs-target="#abrir-modal-ciudad">
+            <div class="add-city">
+                <i class="bi bi-building-fill-add"></i>
+            </div>
+        </a>
+    </div>
+    `;
+
+        
+
+}
+
+function actualizarDepartamentos(){
+    const departamentoOption = document.querySelector('#departamentos');
+    departamentoOption.innerHTML = `<option value="" selected disabled>Seleccione un departamento</option>`;
+    departamentos.forEach(dep =>{
+        departamentoOption.innerHTML += `
+            <option value="${dep.id}">${dep.nomDepartamento}</option>
         `
     });
 }
@@ -163,9 +335,12 @@ function imprimirTabla(datos) {
 async function ActualizarDB() {
     departamentos = await DB.get("departamentos");
     ciudades = await DB.get("ciudades");
-    inmuebles = await DB.get("inmuebles");
 
     DB.LS.getItem('departamentos') ? '' : DB.LS.setItem('departamentos', JSON.stringify(departamentos));
     DB.LS.getItem('ciudades') ? '' : DB.LS.setItem('ciudades', JSON.stringify(ciudades));
-    DB.LS.getItem('inmuebles') ? '' : DB.LS.setItem('inmuebles', JSON.stringify(inmuebles));
+}
+
+
+export {
+    cargarCiudad
 }
--- front/js/clases.js
+++ front/js/clases.js
@@ -2,18 +2,58 @@ export default class DB{
 
     static LS = window.localStorage;
     static apiURL = "http://127.0.0.1:3000/";
+    static apiKeyW = "d35c573092156d5f3633039067cb9cd3";
+
+    static async getWeather(ciudad){
+        const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&units=metric&appid=${this.apiKeyW}`);
+        return res.json();
+    }
     
-    static Delete(coleccion,id){
-        
+    static async delete(coleccion,id){
+        try {
+            console.log('hola')
+            const res = await fetch(this.apiURL + coleccion +`/${id}`, {
+                method: "DELETE"
+            });
+
+            console.log('adeu')
+    
+            if (res.ok) {
+                console.log('eliminado');
+                return true;
+            }
+        } catch (err) {
+            console.error('Hubo un error en la solicitud:', err);
+            return false;
+        }
     }
 
-    static Update(coleccion,id){
-        console.log(coleccion, id);
+    static async update(coleccion, datos, id){
+        try {
+            console.log('hola')
+            const res = await fetch(this.apiURL + coleccion + `/${id}`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(datos)
+            });
+
+            console.log('adeu')
+    
+            if (res.ok) {
+                console.log('eliminado');
+                return true;
+            }
+        } catch (err) {
+            console.error('Hubo un error en la solicitud:', err);
+            return false;
+        }
     }
 
-    static async create(coleccion, datos){
-        try{
-            const res = await fetch(this.apiURL+coleccion, {
+    static async create(coleccion, datos) {
+        try {
+            const res = await fetch(this.apiURL + coleccion, {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
@@ -21,14 +61,18 @@ export default class DB{
                 body: JSON.stringify(datos)
             });
     
-            console.log(res.json());
-            return true;
-
-        }catch(err){
-            console.error('Hubo un error: '+err);
+            if (res.ok) {
+                return true;
+            } else {
+                console.error('Hubo un error en la solicitud:', res.status);
+                return false;
+            }
+        } catch (err) {
+            console.error('Hubo un error en la solicitud:', err);
             return false;
         }
     }
+    
 
     static async get(coleccion){
         try{
